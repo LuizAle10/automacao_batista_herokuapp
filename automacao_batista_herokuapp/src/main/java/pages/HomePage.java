07bba67a96fd7e036e06bf7476dd8721c0ddc1a5
Luiz Alexandre Bispo dos Santos <30711377+LuizAle10@users.noreply.github.com>
2020-08-13 22:04:20 -0300

    Add files via upload

--- automacao_batista_herokuapp/src/main/java/pages/HomePage.java
+++ automacao_batista_herokuapp/src/main/java/pages/HomePage.java
@@ -4,5 +4,12 @@ import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 	private WebDriver driver;
+	
+	public HomePage(WebDriver driver) {
+    	this.driver = driver;
+    	
+    	
+		
+	}
 
 }
